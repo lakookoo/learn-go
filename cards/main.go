81d00e9ee2d970edccb535d0679249766c8e7e18
Larisa <kosareva.larisa@gmail.com>
2024-04-11 20:23:54 +0200

    add custom type and its method

--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var card string = "Ace of Spades"
-	card := newCard()
-	fmt.Println(card)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	cards.print()
+
 }
 
 func newCard() string {
